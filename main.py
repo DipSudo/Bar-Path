829898b4d2d5dc7ea17db54082a53f31ec67ed8d
DipSudo <sudipsun97@gmail.com>
2024-09-05 23:23:49 +0100

    Plot update

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     tracker_key = "csrt"
     output_file = "output_video.mp4"         #Output file name 
     video_title = ("Weight:160kg, Rep:1, RPE:9")
-    process_video(video_file, tracker_key,output_file,video_title)
-    # plot_centroid_path(rel_centroid_path, time)
+    rel_centroid_path, time = process_video(video_file, tracker_key,output_file,video_title)
+    plot_centroid_path(rel_centroid_path, time)
 
 if __name__ == "__main__":
     main()
--- plots.py
+++ plots.py
@@ -8,8 +8,8 @@ def plot_centroid_path(centroid_path,time):
         return
 
     # Extract x and y coordinates
-    x = [coord[0] for coord in centroid_path]
-    y = [coord[1] for coord in centroid_path]
+    x = np.array([coord[0] for coord in centroid_path])
+    y = np.array([coord[1] for coord in centroid_path])
     
     r = np.sqrt(x**2 + y**2)
     t = np.array(time)
@@ -17,9 +17,9 @@ def plot_centroid_path(centroid_path,time):
 
     # Plotting the path
     plt.figure()
-    plt.plot(t, r, marker='o', linestyle='-', color='b')
+    plt.scatter(t, r, marker='o', linestyle='-', color='b')
     plt.title('Centroid Path Over Time')
     plt.xlabel('Time (s)')
-    plt.ylabel('Distance from Origin (r)')
+    plt.ylabel('Radial distance from Origin (px)')
     plt.grid(True)
     plt.show()
